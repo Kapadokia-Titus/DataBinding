a651c6fcc8e0e704088039905d3220ad3e1224a4
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-07-05 01:44:55 +0300

    done simple databinding

--- app/src/main/java/kapadokia/nyandoro/mydatabinding/MainActivity.java
+++ app/src/main/java/kapadokia/nyandoro/mydatabinding/MainActivity.java
@@ -1,14 +1,28 @@
 package kapadokia.nyandoro.mydatabinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import kapadokia.nyandoro.mydatabinding.databinding.ActivityMainBinding;
+import kapadokia.nyandoro.mydatabinding.model.User;
+
 public class MainActivity extends AppCompatActivity {
+
+    // create an instance of the user
+    private User user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding databinding= DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        user = new User();
+        user.setUsername("Kapadokia");
+        user.setEmail("Kapadokia@mail.com");
+
+        databinding.setUser(user);
     }
 }
